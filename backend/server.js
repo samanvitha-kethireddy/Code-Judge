0dfafbeab0e6d25956ede379cbd73e8fc6d3ff64
samanvitha-kethireddy <samanvithareddy07@gmail.com>
2026-06-14 14:10:18 +0530

    deploying

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
+
 
 // app.post("/execute", async (req, res) => {
 //   try {
@@ -458,6 +463,4 @@ app.get("/profile", auth, async (req, res) => {
 });
 
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
-});
+
