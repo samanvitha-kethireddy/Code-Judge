c661233c53e32e89f2bb8a60fa9c4e70afaf1775
samanvitha-kethireddy <samanvithareddy07@gmail.com>
2026-06-14 14:21:29 +0530

    deploying

--- backend/seed.js
+++ backend/seed.js
@@ -1,27 +1,7 @@
-// const mongoose = require("mongoose");
-// const Problem = require("./models/Problem");
-// const problems = require("./problems.json");
-
-// mongoose
-//   .connect("mongodb://localhost:27017/codejudge")
-//   .then(async () => {
-//     await Problem.deleteMany(); // optional
-//     await Problem.insertMany(problems);
-
-//     console.log("Problems inserted successfully");
-//     process.exit();
-//   })
-//   .catch((err) => {
-//     console.error(err);
-//     process.exit(1);
-//   });
-
-
 const mongoose = require("mongoose");
 const Problem = require("./models/Problem");
 const problems = require("./problems.json");
-
-mongoose.connect("mongodb://localhost:27017/onlinejudge")
+mongoose.connect(process.env.MONGO_URI)
   .then(async () => {
 
     for (const problem of problems) {
